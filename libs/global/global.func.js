951afe43f0f8a24013e71d9374a5a6e7f156a93a
yukiweb <liuyuzzia@163.com>
2017-07-03 22:02:30 +0800

    first commit

---
+++ libs/global/global.func.js
@@ -0,0 +1,9 @@
+//这个是 百度蜘蛛抓取的 东西 
+
+
+(function(){
+    var bp = document.createElement('script');
+    bp.src = '../../../push.zhanzhang.baidu.com/push.js'/*tpa=http://push.zhanzhang.baidu.com/push.js*/;
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
